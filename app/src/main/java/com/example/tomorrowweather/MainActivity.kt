224c1ee58312ad4364b845e79b866dc6292faa0b
Wesam Nabeel <wesamezio@gmail.com>
2022-08-02 05:10:32 +0300

    feat: prepare styles, dimensions, colors for the project

--- app/src/main/java/com/example/tomorrowweather/MainActivity.kt
+++ app/src/main/java/com/example/tomorrowweather/MainActivity.kt
@@ -1,25 +1,10 @@
 package com.example.tomorrowweather
 
-import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import com.example.tomorrowweather.databinding.ActivityMainBinding
-import com.example.tomorrowweather.model.repositories.WeatherRepositoryImpl
 import com.example.tomorrowweather.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding =
         ActivityMainBinding::inflate
-    val repository = WeatherRepositoryImpl()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        repository.requestWeatherData() { isSuccess ->
-            if (isSuccess) {
-                Log.i("MAIN_ACTIVITY", "done!")
-                Log.i("MAIN_ACTIVITY",repository.getWeatherData().toString())
-            } else {
-                Log.i("MAIN_ACTIVITY", "failed")
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/tomorrowweather/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tomorrowweather/ui/home/HomeFragment.kt
@@ -1,10 +1,27 @@
 package com.example.tomorrowweather.ui.home
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.example.tomorrowweather.databinding.FragmentHomeBinding
+import com.example.tomorrowweather.model.repositories.WeatherRepositoryImpl
 import com.example.tomorrowweather.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding = FragmentHomeBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val repository = WeatherRepositoryImpl()
+        repository.requestWeatherData() { isSuccess ->
+            if (isSuccess) {
+                Log.i("HOME_FRAGMENT", "done!")
+                Log.i("HOME_FRAGMENT", repository.getWeatherData().timeStamps?.get(0).toString())
+            } else {
+                Log.i("HOME_FRAGMENT", "failed")
+            }
+        }
+    }
 }
